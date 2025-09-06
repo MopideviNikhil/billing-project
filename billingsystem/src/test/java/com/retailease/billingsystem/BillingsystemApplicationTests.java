07033841e00f046244f23c18a3bbbe65e4e360e7
MopideviNikhil <aknikhilmopidevi@gmail.com>
2025-09-06 12:05:48 +0530

    changes

--- billingsystem/src/test/java/com/retailease/billingsystem/BillingsystemApplicationTests.java
+++ billingsystem/src/test/java/com/retailease/billingsystem/BillingsystemApplicationTests.java
@@ -1,10 +1,10 @@
-package in.bushansirgur.billingsoftware;
+package com.retailease.billingsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BillingsoftwareApplicationTests {
+class BillingsystemApplicationTests {
 
 	@Test
 	void contextLoads() {
